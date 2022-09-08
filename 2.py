b529c5721100673b76f3a480574720724a22cea5
Avyy2007 <110321357+Avyy2007@users.noreply.github.com>
2022-09-08 14:22:02 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,3 @@
 '''This is 3rd file'''
 name = input("Type Your Name Here..")
-print("Welcome ", name, " You have successfully integated git into jenkins..."
+print("Welcome ", name, " You have successfully integated git into jenkins...")
